a2594dcf9b1ccf3ec162c808b6765e3c9e760179
Bart <bart@myotis>
2025-03-25 11:20:03 +0100

    add `dynamic_acceleration` function

---
+++ R/dynamic_acceleration.R
@@ -0,0 +1,19 @@
+#' @export
+dynamic_acceleration<-function(x, method=c('odba','vedba')){
+  method<-rlang::arg_match(method)
+  b<-field(x,"bursts")
+  b_centered<-purrr::map(b, ~t(.x)-do.call('c',tapply(t(.x),1:3, mean, simplify=F)))
+  switch(method,
+      "odba"=purrr::map_vec(b_centered, ~mean(.keep_units_optional(colSums,abs(.x)))),
+      "vedba"= purrr::map_vec(b_centered, ~mean(sqrt(.keep_units_optional(colSums,.x^2))))
+      )
+}
+
+# internal function to make keep units only function if it was a unit
+.keep_units_optional<-function(FUN,x,...){
+  if(inherits(x,"units")){
+    units::keep_units(FUN, x,...)
+  }else{
+    do.call(FUN,x,...)
+  }
+}
